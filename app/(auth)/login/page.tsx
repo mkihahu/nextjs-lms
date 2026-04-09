153ef3a25c7d9c46b8f24864665ab49348122652
martin <martin.kihahu@gmail.com>
2026-04-10 01:03:40 +0300

    Implemented sign out

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,86 +1,14 @@
-"use client";
+import { auth } from "@/lib/auth";
+import LoginForm from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { authClient } from "@/lib/auth-client";
-import { IconBrandGithub } from "@tabler/icons-react";
-import { Loader2 } from "lucide-react";
-import { useTransition } from "react";
-import { toast } from "sonner";
-
-export default function LoginPage() {
-  const [githubPending, startGithubTransition] = useTransition();
-
-  async function signInWithGithub() {
-    startGithubTransition(async () => {
-      await authClient.signIn.social({
-        provider: "github",
-        callbackURL: "/",
-        fetchOptions: {
-          onSuccess: () => {
-            toast.success(
-              "Successfully signed in with Github! You will be redirected...",
-            );
-          },
-          onError: (error) => {
-            toast.error(
-              `Failed to sign in with Github: ${error.error.message}`,
-            );
-          },
-        },
-      });
-    });
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    return redirect("/");
   }
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome back!</CardTitle>
-        <CardDescription>
-          Login with your Email or Github account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Button
-          disabled={githubPending}
-          onClick={signInWithGithub}
-          className="w-full"
-          variant="outline"
-        >
-          {githubPending ? (
-            <>
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              Signing in with Github...
-            </>
-          ) : (
-            <>
-              <IconBrandGithub className="size-4" />
-              Sign in with Github
-            </>
-          )}
-        </Button>
-
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">
-            OR
-          </span>
-        </div>
-
-        <div className="grid gap-3">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="you@example.com" />
-          </div>
-        </div>
-        <Button className="w-full mt-4">Continue with email</Button>
-      </CardContent>
-    </Card>
-  );
+  return <LoginForm />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to home page
+          toast.success("Signed out successfully");
+        },
+      },
+    });
+  }
+
   return (
-    <div>
+    <div className="p-20">
       <ThemeToggle />
-      Home page
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
